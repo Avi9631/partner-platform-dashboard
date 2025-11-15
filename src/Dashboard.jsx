869a64c05185ee64904b10bddb4919717028249d
Avi9631 <avikumarshooters@gmail.com>
2025-11-15 14:26:24 +0530

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -50,6 +50,7 @@ function Dashboard() {
           </div>
         </div>
 
+
         {/* Content Section */}
         <div className=" mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
